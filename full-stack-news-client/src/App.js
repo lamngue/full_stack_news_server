7a25d39cf9f55e92bd5c84de9c90288d38d29b0a
Lam Nguyen <lnguye16@gustavus.edu>
2022-04-20 21:51:20 +0700

    Create new layout

--- full-stack-news-client/src/App.js
+++ full-stack-news-client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import AllPosts from './components/allPosts';
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
--- full-stack-news-client/src/components/allPosts.js
+++ full-stack-news-client/src/components/allPosts.js
@@ -1,69 +1,33 @@
 import React from 'react';
 import { Layout, Menu, Breadcrumb } from 'antd';
 import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import { useSelector } from 'react-redux';
 const { Header, Content, Footer, Sider } = Layout;
 const items = [
   {
-    label: 'Navigation One',
-    key: 'mail',
-    icon: <MailOutlined />,
-  },
-  {
-    label: 'Navigation Two',
-    key: 'app',
-    icon: <AppstoreOutlined />,
-    disabled: true,
-  },
-  {
-    label: 'Navigation Three - Submenu',
-    key: 'SubMenu',
+    label: 'Category',
+    key: 'Category',
     icon: <SettingOutlined />,
     children: [
       {
         type: 'group',
         label: 'Item 1',
-        children: [
-          {
-            label: 'Option 1',
-            key: 'setting:1',
-          },
-          {
-            label: 'Option 2',
-            key: 'setting:2',
-          },
-        ],
-      },
-      {
-        type: 'group',
-        label: 'Item 2',
-        children: [
-          {
-            label: 'Option 3',
-            key: 'setting:3',
-          },
-          {
-            label: 'Option 4',
-            key: 'setting:4',
-          },
-        ],
-      },
+      }
     ],
-  },
-  {
-    label: (
-      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-        Navigation Four - Link
-      </a>
-    ),
-    key: 'alipay',
-  },
+  }
 ];
 
 const AllPosts = () => {
-  const [current, setCurrent] = React.useState('mail');
+  const posts = useSelector(state => state.post)
+  const [current, setCurrent] = React.useState('');
+  const [collapsed, setCollapsed] = React.useState(false);
+
   const onClick = (e) => {
-    console.log('click ', e);
     setCurrent(e.key);
+  };
+  const onCollapse = (collapsed) => {
+    console.log(collapsed);
+    setCollapsed(collapsed);
   };
     return (
       <Layout
@@ -71,7 +35,17 @@ const AllPosts = () => {
           minHeight: '100vh',
         }}
       >
-        <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+          <div className="logo" />
+          <Menu theme="dark" onClick={onClick} defaultSelectedKeys={[current]} mode="inline" items={items} />
+        </Sider>
+        <Layout className="site-layout">
+          <Header
+            className="site-layout-background"
+            style={{
+              padding: 0,
+            }}
+          />
           <Content
             style={{
               margin: '0 16px',
@@ -82,8 +56,8 @@ const AllPosts = () => {
                 margin: '16px 0',
               }}
             >
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+              <Breadcrumb.Item>Category</Breadcrumb.Item>
+              <Breadcrumb.Item></Breadcrumb.Item>
             </Breadcrumb>
             <div
               className="site-layout-background"
@@ -102,6 +76,7 @@ const AllPosts = () => {
           >
             Ant Design ©2018 Created by Ant UED
           </Footer>
+        </Layout>
         </Layout>
     );
 }
